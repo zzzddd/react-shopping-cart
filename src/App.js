577b32ce868e2b20344460a173ff640cf6b6fef5
zee <zzdd9251@gmail.com>
2019-10-07 17:56:55 -0700

    inital..

--- src/App.js
+++ src/App.js
@@ -1,42 +1,42 @@
-import React, { useState } from 'react';
-import { Route } from 'react-router-dom';
-import data from './data';
-
+import React, { useState } from "react";
+import { Route } from "react-router-dom";
+import data from "./data";
+import ProductContext from "./contexts/ProductContext";
 // Components
-import Navigation from './components/Navigation';
-import Products from './components/Products';
-import ShoppingCart from './components/ShoppingCart';
+import Navigation from "./components/Navigation";
+import Products from "./components/Products";
+import ShoppingCart from "./components/ShoppingCart";
+import CartContext from "./contexts/CartContext";
 
 function App() {
-	const [products] = useState(data);
-	const [cart, setCart] = useState([]);
+  const [products] = useState(data);
+  const [cart, setCart] = useState([]);
 
-	const addItem = item => {
-		setCart([...cart, item]);
-	};
+  const addItem = item => {
+    // add the given item to the cart
+ setCart([...cart, item]); 
+  };
 
-	return (
-		<div className="App">
-			<Navigation cart={cart} />
+  return (
+    <div className="App">
+      <ProductContext.Provider value={{ products, addItem }}>
+        <CartContext.Provider value={cart}>
+          <Navigation cart={cart} />
 
-			{/* Routes */}
-			<Route
-				exact
-				path="/"
-				render={() => (
-					<Products
-						products={products}
-						addItem={addItem}
-					/>
-				)}
-			/>
+          {/* Routes */}
+          {/* <Route
+          exact
+          path="/"
+          render={() => <Products products={products} addItem={addItem} />}
+        /> */}
+          <Route exact path="/" component={Products} />
+          <Route path="/cart" render={() => <ShoppingCart cart={cart} />} />
 
-			<Route
-				path="/cart"
-				render={() => <ShoppingCart cart={cart} />}
-			/>
-		</div>
-	);
+          {/* <Route path="/cart" ShoppingCart cart={cart}  /> */}
+        </CartContext.Provider>
+      </ProductContext.Provider>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/contexts/ProductContext.js
@@ -0,0 +1,9 @@
+import {createContext} from 'react'
+
+
+const ProductContext = createContext();
+console.log('inside product context...',ProductContext)
+
+
+
+export default ProductContext;
